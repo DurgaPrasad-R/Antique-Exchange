ff1a9fe848d29f80b287f8cae3ddc6c14d761869
Durga Prasad <21pa1a05e8@vishnu.edu.in>
2023-09-11 00:34:28 +0530

    Added index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname+'/public/index.html')
+})
+
+
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname+'/public/login.html')
+})
+
+app.get('/signup', (req, res) => {
+  res.sendFile(__dirname+'/public/signup.html')
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port http://localhost:${port}`)
+})
